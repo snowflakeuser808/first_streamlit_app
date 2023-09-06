e562ee20be287b2717479fd08841dcbb44d14695
snowflakeuser808 <144129694+snowflakeuser808@users.noreply.github.com>
2023-09-06 08:18:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_data_row),['plum'])
-fruit_to_show = my_fruit_list.loc[fruit_selected]
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thank you for adding ', fruit_choice)
